a447538c6dfc6e8e62c469e2a8dd5611cd0f4396
marcojahn060675@gmail.com <marcojahn060675@gmail.com>
2025-02-07 13:27:05 +0100

    Udemy Kurs - Python komplett

---
+++ 2.9 Strings in Python.py
@@ -0,0 +1,4 @@
+age = 22
+# print ("Ich bin " + age + "Jahrae alt.")  # Diese Zeile würde fehlschlagen,weil man bei der String-Ausgabe nicht mehrere Formate mischen
+# darf, sondern den Zahlenwert der Variable umwandeln muss. Stattdessen nutzt man dies hier
+print("Ich bin " + str(age) + " Jahre alt.")
