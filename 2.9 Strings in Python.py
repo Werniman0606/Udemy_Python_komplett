d3b6e6e5b31941995d908e86df887505f9d891ee
Marco Jahn <marcojahn060675@gmail.com>
2025-02-10 18:11:42 +0100

    10.02.2025

--- 2.9 Strings in Python.py
+++ 2.9 Strings in Python.py
@@ -1,4 +1,8 @@
 age = 22
 # print ("Ich bin " + age + "Jahrae alt.")  # Diese Zeile würde fehlschlagen,weil man bei der String-Ausgabe nicht mehrere Formate mischen
-# darf, sondern den Zahlenwert der Variable umwandeln muss. Stattdessen nutzt man dies hier
+# darf, sondern den Zahlenwert der Variable in einen String umwandeln muss. Stattdessen nutzt man dies hier
 print("Ich bin " + str(age) + " Jahre alt.")
+
+# Man kann aber auch eine moderne Darstellungsvariante nutzen, die sogenannten F-Strings. Diese benötigen keine Umwandlung in einen String.
+
+print(f"Ich bin {age} Jahre alt.")
