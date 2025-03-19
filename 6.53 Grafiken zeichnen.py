12b81a20c89ae4efe2540be0a44c17d122eaec1f
marcojahn060675@gmail.com <marcojahn060675@gmail.com>
2025-03-19 15:46:55 +0100

    20.03.2025

---
+++ 6.53 Grafiken zeichnen.py
@@ -0,0 +1,5 @@
+""" In dieser Lektion lernen wir, wie wir eine Grafikzeichnen können. Diese Kenntnisse werden in den folgenden Lektionen gebraucht"""
+
+# Wir müssen dazu ein Modul zum Zeichnen einbinden
+
+%mathplotlib inline
