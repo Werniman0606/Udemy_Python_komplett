fc505e150b937ce2f61d26feea2333cdf44686de
Werniman0606 <56743921+Werniman0606@users.noreply.github.com>
2025-06-03 18:43:48 -0400

    03.05.25

---
+++ 10.104 HTML-Code holen.py
@@ -0,0 +1,5 @@
+# Das Requests-Modul
+# In dieser Lektion lernst du, wie du eine Webseite herunterlädst und die den HTML-Code anzeigen lassen kannst
+# Dazu verwenden wir das requests-Modul (http://docs.python-requests.org/en/master
+# Wir werden folgende Seite crawlen: http://python.beispiel.programmierenlernen.io/index.php
+
